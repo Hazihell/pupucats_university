12ba85dfd9d654f3bcc0d414d046cd7fc1492a72
Roberval Roger Serra da Conceicao <rogerserra.c@gmail.com>
2022-07-07 11:47:47 -0300

    feat(pages): pages nav menu
    It contains links to the parent page and all children pages.
    If a page hasn't or is not a parent, the menu won't show up.

--- wp-content/themes/pupucats_university/page.php
+++ wp-content/themes/pupucats_university/page.php
@@ -17,31 +17,48 @@ while (have_posts()) {
     <div class="container container--narrow page-section">
 
         <?php
+            $parentPageId = wp_get_post_parent_id();
+            $parentPageTitle = get_the_title($parentPageId);
+            $parentPageLink = get_permalink($parentPageId);
+            $isChildPage = $parentPageId > 0;
+            $pageId = get_the_ID();
 
-        $parentPageId = wp_get_post_parent_id();
-        $isChildPage = $parentPageId > 0;
-
-        if ($isChildPage) { ?>
+            if ($isChildPage) { 
+        ?>
             <div class="metabox metabox--position-up metabox--with-home-link">
                 <p>
                     <a 
                         class="metabox__blog-home-link" 
-                        href="<?php echo get_permalink($parentPageId) ?>">
+                        href="<?php echo $parentPageLink ?>">
                         <i class="fa fa-home" aria-hidden="true"></i> 
-                        Back to <?php echo get_the_title($parentPageId) ?>
+                        Back to <?php echo $parentPageTitle ?>
                     </a>
                     <span class="metabox__main"><?php the_title() ?></span>
                 </p>
             </div>
         <?php } ?>
 
-        <!-- <div class="page-links">
-            <h2 class="page-links__title"><a href="#">About Us</a></h2>
-            <ul class="min-list">
-                <li class="current_page_item"><a href="#">Our History</a></li>
-                <li><a href="#">Our Goals</a></li>
-            </ul>
-        </div> -->
+        <?php 
+            $childPagesArray = get_pages([
+                'child_of' => $pageId
+            ]);
+
+
+            if ($isChildPage || $childPagesArray) { 
+        ?>
+            <div class="page-links">
+                <h2 class="page-links__title"><a href="<?php echo $parentPageLink ?>"><?php echo $parentPageTitle ?></a></h2>
+                <ul class="min-list">
+                    <?php 
+                        wp_list_pages([
+                            'title_li' => null,
+                            'child_of' => $isChildPage ? $parentPageId : $pageId,
+                            'sort_column' => 'menu_order'
+                        ]);
+                    ?>
+                </ul>
+            </div>
+        <?php } ?>
 
         <div class="generic-content">
             <?php the_content() ?>
